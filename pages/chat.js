f41ac7222bc5ed901cf6ecb6f9a1fd0803473025
andradeviniicius <viniciusdandrade01@gmail.com>
2022-02-13 17:39:58 -0300

    BoasPraticas component messageList fora do index

--- pages/chat.js
+++ pages/chat.js
@@ -4,6 +4,7 @@ import appConfig from '../config.json';
 import { useRouter } from 'next/router'
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 import { ButtonSendSticker } from '../src/components/buttonSendSticker'
+import { MessageList } from '../src/components/MessageList'
 
 
 import styles from '../styles/glassMorphism.module.css'
@@ -186,84 +187,5 @@ function Header() {
                 />
             </Box>
         </>
-    )
-}
-function MessageList(props) {
-    // const messageList  = props.messageList 
-    var date = new Date("December 17, 1995 03:24:00");
-    return (
-        <Box
-            tag="ul"
-            styleSheet={{
-                overflow: 'auto',
-                display: 'flex',
-                flexDirection: 'column-reverse',
-                flex: 1,
-                color: appConfig.theme.colors.neutrals["000"],
-                marginBottom: '16px',
-            }}
-        >
-            {props.messageList.map((mensagemAtual) => {
-                return (
-                    <Text
-                        key={mensagemAtual.id}
-                        tag="li"
-                        styleSheet={{
-                            borderRadius: '5px',
-                            padding: '6px',
-                            marginBottom: '12px',
-                            hover: {
-                                backgroundColor: appConfig.theme.colors.neutrals[700],
-                            }
-                        }}
-                    >
-                        <Box
-                            styleSheet={{
-                                marginBottom: "8px",
-                                display: "flex",
-                                alignItems: "center"
-                            }}
-                        >
-                            <Image
-                                styleSheet={{
-                                    width: '20px',
-                                    height: '20px',
-                                    borderRadius: '50%',
-                                    display: 'inline-block',
-                                    marginRight: '8px',
-                                }}
-                                src={`https://github.com/${mensagemAtual.de}.png`}
-
-
-                                
-                            />
-                            <Text tag="strong">
-                                {mensagemAtual.de}
-                            </Text>
-                            <Text
-                                styleSheet={{
-                                    fontSize: '10px',
-                                    marginLeft: '8px',
-                                    color: appConfig.theme.colors.neutrals[300],
-                                }}
-                                tag="span"
-                                
-                                >
-                                {mensagemAtual.created_at}
-                            </Text>
-                        </Box>
-                        {mensagemAtual.texto.startsWith(':stickerURL:')
-                            ? (
-                                <Image 
-                                src={mensagemAtual.texto.replace(':stickerURL:', '')} />
-                                )
-                                : (
-                                mensagemAtual.texto
-                                )
-                            }
-                    </Text>
-                )
-            })}
-        </Box>
     )
 }
