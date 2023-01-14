275bd18de4b6ea9609632ae6633b8166ae09f86b
andradeviniicius <viniciusdandrade01@gmail.com>
2023-01-14 19:53:06 -0300

    replace config.json with a TS const

--- theme.ts
+++ theme.ts
@@ -1,16 +1,16 @@
-{
-  "name": "Discord Clone - Coffe break",
-  "stickers": [
+const theme = {
+  name: "Discord Clone - Coffe break",
+  stickers: [
     "http://2.bp.blogspot.com/-d21tffsTIQo/U_H9QjC69gI/AAAAAAAAKqM/wnvOyUr6a_I/s1600/Pikachu%2B2.gif",
     "https://media1.giphy.com/media/BdghqxNFV4efm/200.gif",
     "https://media0.giphy.com/media/fGCa0O9sogzEOTbBaP/giphy.gif?cid=ecf05e47xof8a3exdyrn85zfy3ex1soua3faclst7oqf0fqq&rid=giphy.gif&ct=g",
     "https://i.pinimg.com/originals/0b/1c/23/0b1c2307c83e1ebdeed72e41b9a058ad.gif",
     "https://c.tenor.com/VylWt5lyjBoAAAAC/omg-yes.gif",
-    "https://i.pinimg.com/originals/96/34/c5/9634c520c9a3cd4e7f23190bb2c96500.gif"
+    "https://i.pinimg.com/originals/96/34/c5/9634c520c9a3cd4e7f23190bb2c96500.gif",
   ],
-  "theme": {
-    "colors": {
-      "primary": {
+  theme: {
+    colors: {
+      primary: {
         "050": "#E3F9E5",
         "100": "#89B6E7",
         "200": "#7ba4d1",
@@ -20,9 +20,9 @@
         "600": "#4a627d",
         "700": "#3e536b",
         "800": "#2f4052",
-        "900": "#1f2936"
+        "900": "#1f2936",
       },
-      "neutrals": {
+      neutrals: {
         "000": "#FFFFFF",
         "050": "#F5F7FA",
         "100": "#E4E7EB",
@@ -35,8 +35,10 @@
         "7000": "rgb(33, 41, 49, 0.3)",
         "800": "#181F25",
         "900": "#101418",
-        "999": "#080A0C"
-      }
-    }
-  }
-}
+        "999": "#080A0C}",
+      },
+    },
+  },
+};
+
+export default theme;
