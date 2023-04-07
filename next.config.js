ff288f038219de668430b932e8e4fd0ea754e867
andradeviniicius <viniciusdandrade01@gmail.com>
2023-04-07 16:51:15 -0300

    fix/ update dependencies and config files in the project

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    CLIENT_ID: "Iv1.6fd7b7c524ad7d0e  ",
+    NEXT_PUBLIC_CLIENT_ID: "Iv1.6fd7b7c524ad7d0e",
     APP_ID: "282142",
     CLIENT_SECRET: "e4d3b8c31eb22c033ad873d0deb45c9412123da1",
-    PRIVATE_KEY: "RJTOtqVqKDnpj8LypsXwUIi2d2iefaoy6hzAuKEd7aE"
+    PRIVATE_KEY: "RJTOtqVqKDnpj8LypsXwUIi2d2iefaoy6hzAuKEd7aE",
+  },
+  images: {
+    domains: ["picsum.photos", "github.com"],
   },
 };
 
